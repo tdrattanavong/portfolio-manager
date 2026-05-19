9fd098e9c157aa8e3047fe5478334c10946767e1
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-05-19 13:07:18 -0700

    making models

--- src/main/java/com/pluralsight/models/CreditCard.java
+++ src/main/java/com/pluralsight/models/CreditCard.java
@@ -1,4 +1,45 @@
 package com.pluralsight.models;
 
-public class CreditCard {
+import com.pluralsight.interfaces.Valuable;
+
+public class CreditCard implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public CreditCard(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    @Override
+    public double getValue() {
+        return balance;
+    }
 }
